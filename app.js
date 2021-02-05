b4e9403163b2702a36fca4ff02cc44b1560a304d
shinigamicoder2000 <shinigamicoder2000@gmail.com>
2021-02-05 21:33:04 +0530

    Add user and basic auth

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const ejs=require('ejs');
 const mongoose=require('mongoose');
 const app=express();
 
-mongoose.connect('mongodb://localhost:27017/userDB',{useNewUrlParser:true,useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/userDB1',{useNewUrlParser:true,useUnifiedTopology: true });
 const userSchema={
-    email:String,
-    password:String
+    email:{type:String},
+    password:{type:String}
 };
 const User=new mongoose.model('User',userSchema);
 
@@ -29,6 +29,47 @@ app.get('/login',(req,res)=>{
 app.get('/register',(req,res)=>{
     res.render('register');
 })
+app.post('/register',(req,res)=>{
+
+  const newUser=new User({
+      email:req.body.email,
+      password:req.body.password
+  })
+  newUser.save((err)=>{
+      if(err){
+          console.log(err);
+      }
+      else{
+          console.log('user added');
+          res.render('secrets');
+      }
+  })
+
+
+});
+app.post('/login',(req,res)=>{
+
+     const username=req.body.username;
+     const password=req.body.password;
+
+     User.findOne({email:username}).then((user)=>{
+         if(user){
+             if(user.password===password)
+               {
+                   return res.render('secrets');
+               }
+              
+         }
+         else{
+             res.render('login');
+         }
+     }).catch(err=>{
+         console.log(err);
+     })
+})
 app.listen(8080,(err)=>{
+    if(err)
     console.log(err)
+    else
+    console.log('Server set up on port 8080');
 });
